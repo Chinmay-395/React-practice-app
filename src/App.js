29253254faf97d4b5956ecdadef12edc4b131ce9
Chinmay-395 <chinmaydali395@gmail.com>
2021-02-25 15:29:52 +0530

    The deployment CICD pipeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
 const App = () => {
-  const APP_ID = 'dec8a8fe';
-  const APP_KEY = 'cddf2b90a7b847abff0c308e51abeded';
+  const APP_ID = "dec8a8fe";
+  const APP_KEY = "cddf2b90a7b847abff0c308e51abeded";
   // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState(null);
+
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, []);
 
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = response.json();
-    setRecipes(data.hits)
+    setRecipes(data.hits);
     console.log(data.hits);
-  }
+  };
 
   return (
     <div className="App">
@@ -29,9 +31,9 @@ const App = () => {
           search
         </button>
       </form>
-
+      {recipes == null ? <></> : recipes.map((recipe) => <Recipe />)}
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Recipe = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Calories</p>
+            <img src="..." alt="" />
+        </div>
+    )
+}
+
+export default Recipe;
